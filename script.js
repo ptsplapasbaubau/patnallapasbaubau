4382355ca629d976d4f069c03b5ce679224e926a
ptsplapasbaubau <ptsplapasbaubau@gmail.com>
2026-07-09 22:35:28 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript disiapkan untuk pengembangan lanjutan
+// Contoh: resize iframe otomatis (opsional)
+
+window.addEventListener("load", () => {
+  const iframe = document.getElementById("uploadFrame");
+  console.log("Iframe loaded:", iframe.src);
+});
